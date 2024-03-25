ee2ff7d9182c78211fcab315a10a7ab86634bb71
zakkii-k <59763423+zakkii-k@users.noreply.github.com>
2024-03-25 22:50:22 +0900

    提出ページの文言編集とバックエンドのAPI変更への対応

--- src/api/GetAPI.ts
+++ src/api/GetAPI.ts
@@ -3,10 +3,10 @@ import { Assignment, SubAssignmentDropdown, SubAssignmentDetail } from '../types
 // APIから課題データを取得する関数
 export const fetchAssignments = async (): Promise<Assignment[]> => {
     try {
-        const response = await fetch('http://localhost:8000/sidebar', { mode: 'cors' });
+        const response = await fetch('http://localhost:8000/api/v1/assignments/', { mode: 'cors' });
         const data = await response.json();
         console.log(data);
-        return JSON.parse(data);
+        return data; // 修正箇所
     } catch (error) {
         console.error('課題データの取得に失敗しました', error);
         return [];
@@ -16,10 +16,10 @@ export const fetchAssignments = async (): Promise<Assignment[]> => {
 // APIから課題中の基本課題と発展課題を取得する関数
 export const fetchSubAssignments = async (id: string): Promise<SubAssignmentDropdown[]> => {
     try {
-        const response = await fetch(`http://localhost:8000/assignments/${id}`, { mode: 'cors' });
+        const response = await fetch(`http://localhost:8000/api/v1/assignments/${id}`, { mode: 'cors' });
         const data = await response.json();
         console.log(data);
-        return JSON.parse(data);
+        return data; // 修正箇所
     } catch (error) {
         console.error('課題データの取得に失敗しました', error);
         return [];
@@ -29,10 +29,10 @@ export const fetchSubAssignments = async (id: string): Promise<SubAssignmentDrop
 // APIからドロップダウンで選択された課題の詳細情報を取得する関数
 export const fetchSubAssignmentDetail = async (id: string, sub_id: string): Promise<SubAssignmentDetail | null> => {
     try {
-        const response = await fetch(`http://localhost:8000/assignments/${id}/${sub_id}`, { mode: 'cors' });
+        const response = await fetch(`http://localhost:8000/api/v1/assignments/${id}/${sub_id}`, { mode: 'cors' });
         const data = await response.json();
         console.log(data);
-        return JSON.parse(data);
+        return data; // 修正箇所
     } catch (error) {
         console.error('課題データの取得に失敗しました', error);
         return null;
