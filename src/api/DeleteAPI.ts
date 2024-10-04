c159c7fce9f30f9c91ca2e6bb1661f33f29acf67
Takuya Mizokami <takoyaki65@users.noreply.github.com>
2024-10-04 11:47:57 +0900

    ユーザ削除APIコール関数のデバッグ

--- src/api/DeleteAPI.ts
+++ src/api/DeleteAPI.ts
@@ -3,10 +3,11 @@ import { UserDelete } from '../types/user';
 const API_PREFIX = 'http://localhost:8000/api/v1';
 
 // ユーザーを削除するAPI関数
-export const deleteUsers = async (user_ids: UserDelete, token: string | null): Promise<void> => {
+export const deleteUsers = async (user_ids: string[], token: string | null): Promise<void> => {
     try {
-        const headers = token ? { Authorization: `Bearer ${token}` } : {};
-        await axios.post(`${API_PREFIX}/users/delete`, { user_ids: user_ids.user_ids }, { headers });
+        //console.log('Sending data:', { user_ids: user_ids });
+        const headers = token ? { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' } : {};
+        await axios.post(`${API_PREFIX}/users/delete`, { user_ids: user_ids }, { headers });
     } catch (error) {
         console.error('ユーザーの削除に失敗しました', error);
         throw new Error('ユーザーの削除に失敗しました');
